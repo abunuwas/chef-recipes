73d2b1d4bb2f63c2008e403bd976170541ae4817
abunuwas <joseharoperalta@gmail.com>
2017-07-08 23:45:07 +0100

    Add launch configuration scripts

---
+++ infrastructure/cloudformation_launchconf.sh
@@ -0,0 +1,2 @@
+aws cloudformation create-stack --stack-name CI-Servers --template-body file://cloudformation_launchconf.yaml --parameters \
+    ParameterKey=KeyName,ParameterValue=khurasan --capabilities CAPABILITY_NAMED_IAM
